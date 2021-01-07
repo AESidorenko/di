71d1d856d924beaa4eb321ea67c784cfea6dc0e2
Anton <aesidorenko@yandex.ru>
2021-01-07 10:28:57 +0300

    make bootstrap code simpler

--- index.php
+++ index.php
@@ -2,14 +2,8 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-<<<<<<< HEAD
-=======
 $container = new \AES\DI\Container();
 
 $menu = $container->get('\AES\Example\Menu');
 
-$menu->add(new \AES\Example\MenuItemTypeA());
-$menu->add(new \AES\Example\MenuItemTypeB());
-
 $menu->show();
->>>>>>> feature/add_example_code
