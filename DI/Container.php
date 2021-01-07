14795c364ce7adf6107d4094a3ed4e631f170c2c
Anton <aesidorenko@yandex.ru>
2021-01-07 09:52:02 +0300

    create class skeleton for DI\Container

---
+++ DI/Container.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace AES\DI;
+
+class Container
+{
+    private $items = [];
+
+    // todo: make a factory to create a container
+
+    public function add($item, $parameter): void
+    {
+    }
+
+    public function get($item): object
+    {
+    }
+
+    public function call($method)
+    {
+    }
+}
